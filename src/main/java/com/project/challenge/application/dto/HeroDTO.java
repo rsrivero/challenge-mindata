4b16c1913944f5cc2e5ac290ca3d28f83f71c342
rodri <rodrigo@trackingtime.co>
2022-12-02 16:46:10 -0300

    add liquibase

---
+++ src/main/java/com/project/challenge/application/dto/HeroDTO.java
@@ -0,0 +1,10 @@
+package com.project.challenge.application.dto;
+
+import lombok.Data;
+
+@Data
+public class HeroDTO {
+    private Integer id;
+    private String name;
+    private String power;
+}
